fab203fa16c3c79732cbf543db5d8572d7f8e732
Nino Gjoni <49320058+ngjoni@users.noreply.github.com>
2022-04-30 15:37:12 +0200

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,7 @@ const Map = () => {
           className="rounded"
           frameBorder="0"
           height="500"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2217.699878587617!2d10.085702886331376!3d53.581890916950215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18bf0125ec157%3A0x14ed301c9c89a9d1!2sStephanstra%C3%9Fe%2010%2C%2022047%20Hamburg!5e0!3m2!1sde!2sde!4v1649516136691!5m2!1sde!2sde"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2368.645417744621!2d10.087679500000007!3d53.581944599999986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18b935327ebdb%3A0x6e1585cc4287efb6!2sEduard%20Schmidt%20%26%20Sohn%20GmbH!5e0!3m2!1sde!2sde!4v1651325243801!5m2!1sde!2sde"
           style={{ border: "0" }}
           // tabIndex="0"
           title="Google Maps"
