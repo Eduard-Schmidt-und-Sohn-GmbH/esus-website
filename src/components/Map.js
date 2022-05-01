20ada30f4d8a451cb5b316f612f7830faf525141
Nino G <ngj@posteo.de>
2022-05-01 17:12:05 +0200

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,7 @@ const Map = () => {
           className="rounded"
           frameBorder="0"
           height="500"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2368.645417744621!2d10.087679500000007!3d53.581944599999986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18b935327ebdb%3A0x6e1585cc4287efb6!2sEduard%20Schmidt%20%26%20Sohn%20GmbH!5e0!3m2!1sde!2sde!4v1651325243801!5m2!1sde!2sde"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19180.486239986378!2d10.08600277404263!3d53.581793411922405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18b935327ebdb%3A0x6e1585cc4287efb6!2sEduard%20Schmidt%20%26%20Sohn%20GmbH!5e0!3m2!1sde!2sde!4v1651417874051!5m2!1sde!2sde"
           style={{ border: "0" }}
           // tabIndex="0"
           title="Google Maps"
